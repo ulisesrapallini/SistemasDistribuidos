64da16b967d41f683bfaa509afe1b8b74d095fca
ulises <ulises.rapallini@gmail.com>
2020-10-15 03:45:26 +0000

    ultimo

--- src/project/CristianSyncAlgorithm.java
+++ src/project/CristianSyncAlgorithm.java
@@ -1,13 +1,18 @@
 package project;
 import java.util.ArrayList;
+import java.util.Date;
 
 class CristianSyncAlgorithm implements SyncAlgorithm {
 
-    public void CristianSyncAlgorithm(){
-        
-    } 
+    public void CristianSyncAlgorithm(){} 
     
     public void Syncronized(ArrayList<Machine> listamaquinas){
+        
+        ServerTime serverhora = new ServerTime();
+       
+        Date hora = serverhora.GetTime();
+        
+        listamaquinas.forEach((m) -> { m.setTime( hora ); } );
         
     }
 }
---
+++ src/project/ServerTime.java
@@ -0,0 +1,39 @@
+package project;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+
+import java.util.Date;
+
+class ServerTime {
+  public void ServerTime(){
+      
+  }   
+    
+  public Date GetTime(){
+     /* servidores de Time NTP en argentina
+       server 2.ar.pool.ntp.org
+	   server 3.south-america.pool.ntp.org
+	   server 2.south-america.pool.ntp.org
+     */ 
+     Date date = new Date();
+     try {
+         DatagramSocket socket = new DatagramSocket();
+         InetAddress address = InetAddress.getByName("ntp.cais.rnp.br");
+         byte[] buf = new NTPMessage().toByteArray();
+         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
+         socket.send(packet);
+
+         // Get response
+         socket.receive(packet);
+         byte[] msn = packet.getData();
+         NTPMessage ntp = new NTPMessage(msn);  
+         System.out.println( ntp.receiveTimestamp );
+      }
+      catch(Exception e){
+          
+      }
+      
+  return date;     
+  }
+}
